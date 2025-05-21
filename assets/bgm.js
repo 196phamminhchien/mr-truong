0052cd2ba88570b60f9a25f805826a5154d567fa
Pham Chien <196phamminhchien@gmail.com>
2025-05-21 09:28:31 +0700

    update date

--- assets/bgm.js
+++ assets/bgm.js
@@ -6,10 +6,10 @@ $(function () {
     $('#bgm_pp_btn').on('click', function () {
         const bgm = $('#mcard_bgm').get(0);
         const bgmPanel = $('#bgm');
-        const isPlay = bgm.play;
+        const isPaused = bgm.paused;
 
         if (parseInt(bgmPanel.css('left')) < 0) {
-            if (isPlay) {
+            if (!isPaused) {
                 stopMusic(this, bgm);
             } else {
                 playMusic(this, bgmPanel, bgm);
